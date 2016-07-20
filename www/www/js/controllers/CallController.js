a0a602472ae3879a3286276050e1b82ff9bf3e95
Abdul Jalil <abduljalilm94@gmail.com>
2016-07-20 12:39:56 +0500

    Files added!

---
+++ www/www/js/controllers/CallController.js
@@ -0,0 +1,86 @@
+﻿(function () {
+    angular.module('starter')
+    .controller('CallController', ['localStorageService', '$scope', '$ionicPopup', CallController]);
+
+    function CallController(localStorageService, $scope, $ionicPopup) {
+
+        $scope.username = localStorageService.get('username');
+
+        var peer = new Peer($scope.username, {
+            key: '18dend7xxp5sif6r',
+            config: {
+                'iceServers': [
+                  { url: 'stun:stun1.l.google.com:19302' },
+                  { url: 'turn:numb.viagenie.ca', credential: 'xerox', username: 'abduljalilm94@gmail.com' }
+                ]
+            }
+        });
+
+        function getAudio(successCallback, errorCallback) {
+            navigator.webkitGetUserMedia({ audio: true, video: false }, successCallback, errorCallback);
+
+        }
+
+
+        function onReceiveCall(call) {
+
+            $ionicPopup.alert({
+                title: 'Incoming Call',
+                template: 'Someone is calling you. Connecting now..'
+            });
+
+            getAudio(
+                function (MediaStream) {
+                    call.answer(MediaStream);
+                },
+                function (err) {
+                    $ionicPopup.alert({
+                        title: 'Error',
+                        template: 'An error occurred while try to connect to the device mic'
+                    });
+                }
+            );
+
+            call.on('stream', onReceiveStream);
+        }
+
+
+        function onReceiveStream(stream) {
+            var audio = document.getElementById('contact-audio');
+            audio.src = window.URL.createObjectURL(stream);
+            audio.onloadedmetadata = function (e) {
+                audio.play();
+                $ionicPopup.alert({
+                    title: 'Call Ongoing',
+                    template: 'Call has started. You can speak now'
+                });
+            }
+
+        }
+
+        $scope.startCall = function () {
+            var contact_username = $scope.contact_username;
+
+            getAudio(
+                function (MediaStream) {
+
+                    var call = peer.call(contact_username, MediaStream);
+                    call.on('stream', onReceiveStream);
+                },
+                function (err) {
+                    $ionicPopup.alert({
+                        title: 'Error',
+                        template: 'An error occurred while try to connect to the device mic'
+                    });
+                }
+            );
+
+        };
+
+        peer.on('call', onReceiveCall);
+
+
+
+    }
+
+})();
---
+++ www/www/js/controllers/IndexController.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    angular.module('starter')
+    .controller('IndexController', ['localStorageService', '$scope', '$state', IndexController]);
+
+    function IndexController(localStorageService, $scope, $state) {
+
+        $scope.login = function () {
+
+            var username = $scope.username;
+            localStorageService.set('username', username);
+            $state.go('app');
+
+        };
+
+    }
+
+})();
